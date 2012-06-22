6a7ad7c5cb02734a619620fc4f2d97c0d74d14e1
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-06-22 16:08:15 +0200

    Transfomrations between polar and cartesian coordinates

--- src/main/java/com/oschrenk/humangeo/cs/PolarCoordinateSystem.java
+++ src/main/java/com/oschrenk/humangeo/cs/PolarCoordinateSystem.java
@@ -1,5 +1,9 @@
 package com.oschrenk.humangeo.cs;
 
+import com.oschrenk.humangeo.core.Cartesian2dCoordinate;
+import com.oschrenk.humangeo.core.PolarCoordinate;
+
+// TODO: Auto-generated Javadoc
 /**
  * The polar coordinate system is a two-dimensional coordinate system in which
  * each point on a plane is determined by a distance from a fixed point and an
@@ -28,4 +32,34 @@ public class PolarCoordinateSystem {
 		return 2;
 	}
 
+	/**
+	 * Transforms a polar coordinate to cartesian coordinate.
+	 * 
+	 * @param polarCoordinate
+	 *            the polar coordinate
+	 * @return the cartesian coordinate
+	 */
+	public static Cartesian2dCoordinate toCartesianCoordinate(
+			final PolarCoordinate polarCoordinate) {
+		return new Cartesian2dCoordinate(polarCoordinate.getR()
+				* Math.cos(Math.toRadians(polarCoordinate.getTheta())),
+				polarCoordinate.getR()
+						* Math.sin(Math.toRadians(polarCoordinate.getTheta())));
+	}
+
+	/**
+	 * Transforms a cartesian coordinate to polar coordinate.
+	 * 
+	 * 
+	 * @param cartesianCoordinate
+	 *            the cartesian coordinate
+	 * @return the polar coordinate
+	 */
+	public static PolarCoordinate fromCartesianCoordinate(
+			final Cartesian2dCoordinate cartesianCoordinate) {
+		return new PolarCoordinate(Math.sqrt(cartesianCoordinate.getX()
+				* cartesianCoordinate.getX() + cartesianCoordinate.getY()
+				* cartesianCoordinate.getY()), Math.atan2(
+				cartesianCoordinate.getY(), cartesianCoordinate.getX()));
+	}
 }
