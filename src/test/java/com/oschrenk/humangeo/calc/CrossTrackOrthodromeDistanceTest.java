6ab613e13cd2a0db385c003e4b042cfdd9e50fcb
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-07-04 18:36:34 +0200

    Add test for cross track distance from (45,45) to prime meridian
    Test fails for now. Trying to get cross validation for my assumptions.
    Added FIXME tag

--- src/test/java/com/oschrenk/humangeo/calc/CrossTrackOrthodromeDistanceTest.java
+++ src/test/java/com/oschrenk/humangeo/calc/CrossTrackOrthodromeDistanceTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 import com.oschrenk.humangeo.core.Segment;
 import com.oschrenk.humangeo.cs.Geographic2dCoordinate;
+import com.oschrenk.humangeo.cs.GeographicCoordinateSystem;
+import com.oschrenk.humangeo.ref.Ellipsoids;
 import com.oschrenk.humangeo.ref.Spheres;
 
 /**
@@ -37,5 +39,26 @@ public class CrossTrackOrthodromeDistanceTest {
 				* Spheres.EARTH.getRadius();
 
 		assertEquals(circumferenceEarth / 8, distance, 0.000000001d);
+	}
+
+	@Test
+	// FIXME validate result; implmentation says 3335847.799336763, I say
+	// 3538200.8998758686
+	public void testPointOn45() {
+		final Geographic2dCoordinate from = new Geographic2dCoordinate(0, 0);
+		final Geographic2dCoordinate to = new Geographic2dCoordinate(90, 0);
+		final Geographic2dCoordinate point = new Geographic2dCoordinate(45, 45);
+		final double distance = new CrossTrackOrthodromeDistance(Spheres.EARTH)
+				.distance(point, new Segment<Geographic2dCoordinate>(from, to));
+
+		GeographicCoordinateSystem geographicCoordinateSystem = new GeographicCoordinateSystem(
+				Ellipsoids.WGS_84, Spheres.EARTH);
+		double radiusOfLatitudeOnAuxiliarySphere = geographicCoordinateSystem
+				.getRadiusOfLatitudeOnAuxiliarySphere(45);
+
+		final double circumference = 2 * Math.PI
+				* radiusOfLatitudeOnAuxiliarySphere;
+
+		assertEquals(circumference / 8, distance, 0.000000001d);
 	}
 }
